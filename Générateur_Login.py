1feff5766d8c807656936a2e5bce5fa3206a011d
Eldrak1911 <wilhelmpflieger@gmail.com>
2024-03-24 14:32:24 +0100

    V2

--- Générateur_Login.py
+++ Générateur_Login.py
@@ -2,12 +2,13 @@ def LOGIN_GEN():
     name = ""
     first_name = ""
     login = ""
+    longeur = int(input("Veuillez entrer la longeur du login souhaitée : "))
     name = input("Veuillez entrer votre nom de famille : ")
     first_name = input("Veuillez entrer votre prénom : ")
     print(first_name, name, "voici votre login : ")
-
-    login = (first_name[0])+"."+ name[0]+name[1]+name[2]+name[3]
-
-    print(login.lower())
+    
+    login = (first_name[0])+"."+name
+    
+    print(login[:longeur].lower())
     
 LOGIN_GEN()
