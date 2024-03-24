e05061301f7ec0c4a334abf080998dd657a232e6
Eldrak1911 <wilhelmpflieger@gmail.com>
2024-03-24 14:18:37 +0100

    V2

--- Générateur_MDP.py
+++ Générateur_MDP.py
@@ -1,14 +1,16 @@
-def MDP_GEN():
+def MDPGEN():
     import random
 
     LETTRES = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     lettres = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     chiffres = ["0","1","2","3","4","5","6","7","8","9"]
-    Special = ["@","#","-","_","%","*","‰","€","$","£","µ","?","^"]
-    MDP = ""
-    print("Ce générateur de mot de passe créer des chaînes de 12 caractères.")
+    Special = ["@","#","-","","%","*","‰","€","$","£","µ","?","^"]
+    MDP = [""]
+    mdp_final = ""
+    longeur = int(input("Veuillez entrer le nombre de caractères souhaités : "))
+    print("Ce générateur de mot de passe créera des chaînes de {longeur} caractères.")
 
-    while (len(MDP) < 12):
+    while (len(MDP) < longeur):
         a = random.choice(LETTRES)
         MDP += a
         c = random.choice(chiffres)
@@ -18,4 +20,9 @@ def MDP_GEN():
         b = random.choice(lettres)
         MDP += b
     else:
-        print(MDP)
+        random.shuffle(MDP)
+        for i in MDP:
+            mdp_final += i
+        print(mdp_final)
+
+MDPGEN()
